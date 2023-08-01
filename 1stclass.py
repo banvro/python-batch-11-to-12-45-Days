a47c9e0b8275b61796430169fb199c8e627f6572
Naresh Kumar <bnavro07@gmail.com>
2023-08-01 11:52:39 +0530

    First class

---
+++ 1stclass.py
@@ -0,0 +1,154 @@
+# Python : 
+    # 1) simple
+    # 2) Gernal Perpose
+    # 3) High Lavel
+    # 4) Interpreted
+    # 5) Object Oriented
+    # 7) Dynmiclly typed
+    # 8) Case senstive langauge...
+
+
+# 1) Simple: 
+
+# # include <stdio.h>
+# # include <conio.h>
+
+# int main(){
+#     int a, b;
+
+#     a = 10;
+#     b = 20
+#     int c = a + b;
+
+#     printf("%d", c)
+# }
+
+# python
+
+# a = 10
+# b = 20
+
+# c = a + b
+# print(c)
+
+# print(10 + 20)
+
+
+# 2) Gernal Perpose : it is used for various perpose:
+    # a) Web devlopment
+            # Django, Flask
+
+    # b) Mobile apps backed devlopment
+            # DRF ( DJango Rest Framework) (API)
+
+    # c) AI 
+
+# 3.) High Lavel
+
+# a) High lavel language
+
+# b) Low lavel language.
+    # (Machine language)    01
+
+
+# 5
+# 8421
+# 0101
+
+
+# Interpreted
+
+
+# a) interpreter
+
+
+
+# b) compiler 
+
+
+#  Object Oriented (OOPs)
+    # 1) class 
+    # 2) object
+    # 3) inharitance
+    # 4) polymorphisum
+    # 5) Encapsulation
+    # 6) Abstraction
+
+
+# 6) Dynmiclly typed:  
+
+# int a;
+
+    # a = "askdajksd"
+
+
+
+# 8) Case senstive:
+
+# a = 10
+
+# A = 20
+
+# age = 30
+
+# Age = 20
+
+
+
+
+
+# ***********************************************************/
+
+
+# Variables : are used to store something...
+# rules:
+
+# 1) Variable always start from charcter or undescore.
+# 2) variable never start from inteeger or any symbol
+# 3) variable never contain space 
+# 4) Variable never a keyword
+
+# abc = 20
+
+# _ac = 49
+
+
+# 1abc = 30
+
+# %au = 50
+
+# a b = 30
+
+#  qw = 78
+
+
+
+# comments: to increse code readiablity
+
+# 1) Single line comment
+
+#   skdfjskdfjsdf sdfjbsdkf sdfk dsf dskf dskf
+
+# 2) Multi line commnent
+
+"""
+skfjdskdfskdfbksdf
+sdfkj dskfdskf dskf
+sdf dskf ksdfdskf
+"""
+
+# print("Hello world!")
+# print()
+# input()
+
+
+
+
+# python defination
+# comments
+# variables
+
+
+
+# diffrece between compiler or interpreated langauges..
+# python defination.....
