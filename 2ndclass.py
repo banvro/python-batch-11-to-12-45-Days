27a235640833e13710934de6c59bd1ea10af836c
Naresh Kumar <bnavro07@gmail.com>
2023-08-02 12:39:20 +0530

    Second class

---
+++ 2ndclass.py
@@ -0,0 +1,126 @@
+# print() => to print anything on console..
+# input() => to get input from user...
+
+# print("Hello word")
+
+
+# data types:
+# 1) integer
+# 2) Float
+# 3) String 
+# 4) Boolean
+# 5) Complex 
+# 6) List 
+# 7) tuple
+# 8) Dictionary
+# 9) Set
+
+
+
+
+
+
+
+
+# a = 1 + 2j
+
+# to check data type of a variable: type()
+
+# print(type(a))
+# a = 20
+
+# print("a", a, "xyzzz")
+
+# a = 10
+
+# b = 20
+
+# c = a + b
+
+# print(" the sum is : ",c)
+
+
+# input()
+# str
+# name = input("Enter your name : ")
+
+# print(name)
+
+# concatination => to join two string..
+
+# num1 = int(input("Enter first number : "))
+# num2 = int(input("ENter second number : "))
+
+# sum = num1 + num2
+
+# print("The sum is : ", sum)
+
+
+# print()
+# input()
+
+
+
+
+# statements
+
+# if else elif 
+
+
+
+
+
+
+
+
+age = int(input("Enter your age : "))
+
+# true or false
+
+# indentation
+
+# 11 == 10
+# if age == 18:
+#     print("welcome, this is your first voat..")
+
+# elif age > 18:
+#     print("You are eligibal for voat")
+
+# else:
+#     print( "you are not eligible for voat")
+    
+    
+
+# *************** Calculator *********************
+
+# num1 = user input
+# num2 = user input
+
+#       Enter what you want to do..
+      
+#       1) Adation
+#       2) Subtraction
+#       3) Multiplication
+#       4) Devsion
+      
+# choice = input...
+
+
+
+# if choice == "1":
+    
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
