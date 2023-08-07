9be0cc7315846f8d36d13459d80a8d46c06d60a1
Naresh Kumar <bnavro07@gmail.com>
2023-08-07 11:38:33 +0530

    Python list, set, tuple, dictionry

---
+++ 5thclass.py
@@ -0,0 +1,129 @@
+# LIST, Tuple, Set, Dictionry
+
+# a = 10
+
+
+# lst = [12, 23, 34, "Hwlo", "12", 12.0, 100]
+
+# print(type(lst))
+
+# lst.append("newdata")
+
+# lst.insert(1, "testdata")
+
+# print(lst)
+
+# # len()
+
+# print(len(lst))
+
+# lst.pop()
+
+# print(lst)
+
+# slicin
+# lst = [12, 23, 34, "Hwlo", "12", 12.0, 100]
+
+# print(lst[start, end, increment])
+
+
+# print(lst[2:5])
+# print(lst[5:7])
+
+
+
+# Tuple:
+# ()
+
+# tp = (12, 20, "name", "car", 12.0)
+
+# print(tp)
+
+# print(type(tp))
+
+# print(tp[2:4])
+
+
+
+
+# Set
+# {}
+
+# st = {12, "data", "100", 20, 90, 400}
+
+# # unordered
+
+# # print(st)
+
+# # print(type(st))
+
+# st.add("new data")
+
+# st.remove(90)
+
+# print(st)
+
+
+
+# Dicionry => key:value pairs
+
+
+# dict = {
+#     "name" : "Kriss", 
+#     "Age" : 20, 
+#     "Marks" : 90
+# }
+
+# print(dict)
+
+
+# print(dict["name"])
+
+
+# print(dict.keys())
+# print(dict.values())
+
+
+# dict["phone_number"] = 92938492836
+
+# print(dict)
+
+# mutable => Changeable
+
+# LIST : Ordered, Mutable
+
+
+# Tuple : Ordered, imutable
+
+
+# Set : Unordereed, mutable
+    
+    
+# Dictionry : Ordered, mutable
+
+
+
+# tupl = (12, 34, 90, "hlo", "car")
+
+# lst = list(tupl)
+
+# lst.append(12)
+
+# tupl = tuple(lst)
+
+# print(tupl)
+
+
+
+
+
+# a = 10
+
+# b = str(a)
+
+
+# print(b)
+# print(type(b))
+
+
+
