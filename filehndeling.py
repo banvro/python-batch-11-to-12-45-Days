59b6c2195c44daa01b17c8a2dab789af3fa009f3
Naresh Kumar <banvro07@gmail.com>
2023-08-09 06:45:44 +0530

    class content

---
+++ filehndeling.py
@@ -0,0 +1,49 @@
+# File Handeling : 
+# a = 10
+# b = 20
+# sum = a + b
+
+# fil = open('hello.txt', 'r')
+
+# # print(fil.read())
+
+# print(fil.readline())
+# print(fil.readline())
+
+
+
+# writing in file 
+
+# fil = open('xyz.txt', 'w')
+
+# fil.write("i am new..")
+
+# a => append
+
+# fil = open('xyzw.txt', 'a')
+# fil.write("  this is new data")
+# fil.close()
+
+# fil = open("myfolder/newfil.txt", 'r')
+# print(fil.read())
+
+
+
+# a = 20
+# b = 30
+
+# c = a + b
+
+# newvar = str(c)
+
+
+# file = open("savedata.txt", 'w')
+# file.write(f"The sum is : {newvar}")
+
+
+# "the outpou is : 50
+
+
+
+
+
