9caa739bf357ccbfbdee3a2c7192c689e81e67ef
Naresh Kumar <banvro07@gmail.com>
2023-08-11 11:31:54 +0530

    Abstraction or encapsulation

---
+++ oops/abstraction.py
@@ -0,0 +1,94 @@
+# OOps:
+    # 1) Inharitance
+    # 2) Abstraction
+    # 3) ENcapsulation
+    # 4) Polymrphisum
+    
+# objects
+# claases
+# self
+# constructer
+# super()
+
+
+# Abstraction : data hinding  
+
+# from abc import ABC, abstractmethod
+# # abc => abstract base class
+
+# # interface
+# class clas1(ABC):
+#     @abstractmethod
+#     def car(self):
+#         print("This is a car")
+    
+#     def bus(self):
+#         pass
+    
+#     @abstractmethod
+#     def plane(self):
+#         pass
+
+# class clas2(clas1):
+#     def mthd1(self):
+#         print("i am ok")
+    
+#     def car(self):
+#         print("ok")
+    
+#     def plane(self):
+#         print("Done")
+
+# obj = clas2()
+# obj.car()
+
+
+# from abc import ABC, abstractmethod
+
+# class fields(ABC):
+#     @abstractmethod
+#     def name(self):
+#         pass
+    
+#     @abstractmethod
+#     def age(self):
+#         pass
+    
+#     @abstractmethod
+#     def phonenumber(self):
+#         pass
+
+# class filldata(fields):
+#     def done(self):
+#         print("no")
+    
+#     def name(self):
+#         print("this is name")
+
+#     def age(self):
+#         print("this is age")
+    
+#     def phonenumber(self):
+#         print("this is phonenumber")
+    
+
+# obj = filldata()
+
+
+
+
+
+# Encapsulation : binding up data in a single unit..
+
+# class xyz:
+#     def abc(self):
+#         print("nooo")
+    
+#     def car(self):
+#         print("done....")
+
+
+
+
+
+
