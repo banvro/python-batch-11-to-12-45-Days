19437d05a7dc604e5098f9a19cf3d867ff2c380d
Naresh Kumar <banvro07@gmail.com>
2023-08-16 11:33:04 +0530

    Exception handeling

---
+++ Exception_handeling.py
@@ -0,0 +1,104 @@
+# Exception Handeling : 
+
+# a = 10
+
+# b = 0
+
+# print(a / b)
+
+# a = int(input("Enter you age : "))
+
+    # 1) Compile time error
+    # 2) Logical Error
+    # 3) Run time error
+    
+    
+# 1) Compile time error
+
+# compile time error   =>  SyntaxError
+
+# for i in range(12, 20)
+#     print(i)
+
+# 2) Logical Error :
+
+
+# a = 10
+# b = 0
+# print(a / b)
+
+
+# 2) Runtime error :
+
+# age = int(input("Enter your age : "))
+
+
+# try:
+    
+# except
+
+# a = 10
+
+# b = 0
+
+# try:
+#     c = a / b
+#     print(c)
+    
+# except Exception:
+#     print("noooo")
+    
+# print("i am running....")
+
+# print("i am done")
+
+
+
+# print("User Age")
+
+# try:
+#     age = int(input("Enter your age : "))
+    
+#     print("My age is : ", age)
+
+# except Exception:
+#     print("noooooooooooooooo")
+
+# print("running....")
+
+# print("DOneeee")
+
+
+
+# a = 10
+# b = 0
+
+
+# try:
+#     age = int(input("ENter age : "))
+#     print("My age is : ", age)
+#     c = a / b 
+#     print(c)
+
+
+# except ValueError as e:
+#     print(e)
+
+# except ZeroDivisionError as e:
+#     print(e)
+
+# except Exception as e:
+#     print(e)
+
+
+# print("ooooooooooooooooooooooooooo")
+
+
+
+
+
+
+
+
+
+
