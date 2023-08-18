f9d1bc3ec95c851b87de4529357c326259fb6c63
Naresh Kumar <banvro07@gmail.com>
2023-08-18 12:05:59 +0530

    reguler expression

--- reguler_expression.py
+++ reguler_expression.py
@@ -26,14 +26,22 @@ import re
 
 
 
-text = "CarDekho brings a complete range of new cars in 2023 in India with prices. 9283627829 You can search cars by applying filters such as by price, by bodytype, by brand, by seating capacity & more. 9286102835 Also, stay updated with 8273927308coming cars, electric cars in India, compare cars in your price range and stay tuned to  9272873621the latest car news."
+text = "CarDekho brings a complete range of new cars in 2023 in India with prices. 9283627829 resmYou can search cars by applying filters such as by price, by bodytype, by brand, by seating capacity & more. rkkm 9286102835 Also, stay updated with 8273927308coming cars, electric cars in India, compare cars in your price range and stay tuned to  9272873621the latest car news. room"
 
 
-ptrn = "aaaaaaaa"
+# ptrn = "aaaaaaaa"
 
-# mtch = re.sub(ptrn, "aaaaaaaa", text)
-mtch = re.match(ptrn, text)
+# # mtch = re.sub(ptrn, "aaaaaaaa", text)
+# mtch = re.match(ptrn, text)
 
-print(mtch)
+# print(mtch)
 
+
+# WILDCARD
+
+ptrn = "r..m"
+
+math = re.findall(ptrn, text)
+
+print(math)
 
