8eda438084fcb9f375e9a8055a76d3ec1776b884
Naresh Kumar <banvro07@gmail.com>
2023-08-21 23:11:19 -0700

    start nysql

---
+++ startmysql.py
@@ -0,0 +1,50 @@
+# DataBase : 
+    # Mysql
+    # Postgrace
+    # SQllite
+
+# sql = structured query language
+
+# 1) DDl
+# 2) DML
+# 3) DCl
+# 4) TCL
+
+
+# to install mysql connector the command is : 
+
+    # pip install mysql-connector-python
+
+import mysql.connector
+
+conwc = mysql.connector.connect(host = "localhost", user = "root", password = "abc@123", database = "phone", auth_plugin='mysql_native_password')
+
+
+# if conwc.is_connected():
+#     print("database connected")
+
+couser = conwc.cursor()
+
+# z = couser.execute('create database laptop;')
+z = couser.execute('insert into mobileinfo values(1, "Apple", "Red", 10000);')
+
+
+conwc.commit()
+
+
+
+
+
+# show databases;
+
+# create database phone;
+
+# use phone;
+
+# create table mobileinfo(mobile_s_nmbr INT, mobile_name varchar(50), mobile_color varchar(30), mobile_price decimal);
+
+# insert into mobileinfo values(1, "Apple", "Red", 10000);
+
+# show tables;
+
+# select * from mobileinfo;
