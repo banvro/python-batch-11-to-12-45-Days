217f1744ac96c01945701f47823f65718bbf6ef2
Naresh Kumar <banvro07@gmail.com>
2023-09-20 04:43:01 -0700

    python if else or elif

---
+++ september-batch-python/if-elif-else.py
@@ -0,0 +1,59 @@
+# control statemnets : are used to control flow of program execution....
+
+# if elif else
+
+age = 19
+
+
+# True / Flase
+# 19 == 20
+# elif
+
+
+# if age == 100:
+#     print("Hello world")
+    
+# else:
+#     print("i am elase...")
+
+# voating system
+
+# age = int(input("Enter your age : "))
+# # 19 > 18
+# if age >= 18:
+#     print("you are eligible for voat")
+
+# elif age == 18:
+#     print("Welcome this is your first voat.")
+
+# elif age == 19:
+#     print("this is your second voat")
+    
+# else:
+#     print("Your are not eligible for voat")
+    
+
+
+
+
+# Nested if else : 
+
+
+age = int(input("Enter your age : "))
+# 19 > 18
+# if age <= 18:
+#     if age == 18:
+#         print("Welcome this is your first voat.")
+#     else:
+#         print("you ok")
+
+# elif age <= 19:
+#     if age > 18:
+#         print("hello")
+#     elif age == 20:
+#         print("car")
+#     else:
+#         print("this is your second voat")
+    
+# else:
+#     print("Your are not eligible for voat")
