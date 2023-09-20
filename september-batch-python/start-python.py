f2f71ceff0df70bf1944fd57fadb37a05a3efcf4
Naresh Kumar <banvro07@gmail.com>
2023-09-20 04:41:39 -0700

    Start Python

---
+++ september-batch-python/start-python.py
@@ -0,0 +1,116 @@
+# python  
+
+# variables : are use to hold values
+
+# 1) variables always strat from charchter or underscore.
+# 2) varibale  never strat from space, any spacil symbole or any integer
+# 3) varibale never be a keyword
+
+# we = 10
+
+# first_name = "kriss"  
+
+# keywrods :  int
+
+# int = 20
+# int , type, float
+
+
+# Data types 
+# 1) integer  :   1, 2, 3, -1, 89, 100,
+# 2) Float : 100.8, 90.9, 10.0
+# 3) string :
+    # "12"
+    # 'this is ok'
+    # """this is also a string"""
+# 4) boolean :  True , False
+# 5) complex  : a + bj
+                # 10 + 20j
+
+
+# a = 10
+# 6) list 
+    # lst = [12, 10, 200, 90, "helo", 100.89, 100]
+# 7) tuple
+    # tpl = (12, 100, "helo")
+
+# 8) set 
+    # st = {12, 100, 20, 800}
+
+# 9) dictionry
+    # {key : value }
+    # dict = {"name" : "hlo", "age" : 30}
+
+
+# Functions : 
+# 1) user define functions 
+
+# 2) built in function
+
+# xyz()
+
+# 1) print() => use to print anything on console
+# 2) input() => to get input from user
+# 3) len()  => to get len or number of elements
+# 4) type() => to know about the data typle of a variable..
+
+# a = 10
+
+# print("Hellow world a", a, "this is a new text")
+
+
+# input()
+# string 
+# username = input("Enter your Name : ")
+
+# print(username)
+
+
+# a = 10
+# b = 20
+
+# c = a + b
+
+# print(c)
+
+# concatenation : join/sum of two strings
+
+# a = int(input("ENter first number : "))
+# b = int(input("Enter second number : "))
+
+# c = a + b
+
+# print("the sum is : ", c)
+
+
+# type() 
+
+# a = {10 : "hlo", 20 : "no"}
+# print(type(a))
+
+# len()
+
+# a = [19826391823, 20, 80, 67]
+# print(len(a))
+
+
+
+# age = 10
+
+
+
+# help(print)
+
+"""
+
+askjdb
+"""
+
+
+# int 
+# int()
+
+# import math 
+
+# # print(math.pow(2, 3))
+# print(math.sqrt(30))
