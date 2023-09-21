4019ed8a56aadc1e844b4e024a21be6c188a78fe
Naresh Kumar <banvro07@gmail.com>
2023-09-21 00:23:32 -0700

    loops done

---
+++ september-batch-python/loops.py
@@ -0,0 +1,184 @@
+# Loops : ittrations
+
+# a = "3alk wj"
+
+# # for loop 
+
+# for u in a:
+#     print(u)
+#     print("hello")
+#     print(u, u, "heyyyyyyy")
+
+
+# range(start, end, increament)
+
+# default values 
+
+# start = 0
+# end = n - 1
+# increamnet = 1
+
+
+# for i in range(2, 10, 2):
+#     print(i)
+
+
+# a = 2832834698234689
+ 
+# for i in a:
+#     print(i)
+
+
+
+
+# 2 x 1 = 2
+# 2 x 2 = 4
+# x x 3 = 6
+# .
+
+# ..
+
+# for i in range(1, 11):
+#     print("2 x", i, "=", 2*i)
+
+
+# a = [12, 100, 78, 6]
+
+# for i in a:
+#     print(i)
+
+# while loop :
+
+# age = 10
+
+# 10 < 100
+# while age < 100:
+#     age = age + 10
+#     print("heloooo....", age)
+
+
+
+
+# print("2 x ", i, "=", 2*i)
+
+# i = 1
+# while i < 11:
+#     print("2 x ", i, "=", 2*i)
+#     i = i + 1
+
+
+
+# for i in range(8):
+#     # pass
+#     if i == 4:
+#         # break
+#         # continue
+#         pass
+#     print(i)
+
+
+
+# assert 
+
+# assert 10 == 11 , "this is not equal"
+
+
+
+# type casting  
+
+# int 
+# float
+# str 
+# bool
+# tuple
+# list
+
+# a = 10
+# print(a, type(a))
+
+# b = float(a)
+# print(b, type(b))
+
+# c = str(a)
+# print(c, type(c))
+
+# a = "10"
+
+# c = int(a)
+# print(c, t ype(c))
+
+
+
+# lst = [12, 100, 67, 56]
+
+# tp = tuple(lst)
+
+# print(tp)
+
+
+
+
+# abc = 1231
+
+# 7
+
+# abc = 1928348 932469234
+# zx = str(abc)
+# totl = 0
+# for i in zx:
+#     totl = totl + int(i)
+
+# print(totl)
+
+
+# 0, 1, 1, 2, 3, 5, 8, 13, 21, 34 
+
+# a = 0 
+# b = 1
+
+# while a < 35:
+#     print(a)  # 0, 1, 1, 2
+#     c = a + b    # 1, 2, 3, 5
+#     a = b  # 1, 1, 2, 3
+#     b = c # 1, 2, 3, 5
+
+
+
+
+
+
+
+
+# ab = "abcde1fghi2kajsd1ajksd2kjbk21kjbjkvk12vkv1221kjvjkv12"
+
+# 1, 2
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
