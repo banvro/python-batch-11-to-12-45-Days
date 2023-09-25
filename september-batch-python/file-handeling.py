7521e441e04faf770278c9c731b159a1e4bc5cb9
Naresh Kumar <banvro07@gmail.com>
2023-09-24 22:56:55 -0700

    file hndeling

---
+++ september-batch-python/file-handeling.py
@@ -0,0 +1,30 @@
+# a = 10
+# b = 20
+# c = a + b
+
+# print(c)
+
+
+
+# fil = open("test.txt", 'r')
+# # print(fil.read())
+# print(fil.readline())
+# print(fil.readline())
+# fil.close()
+
+
+# fil = open("testxyz.txt", 'w')
+# fil.write("this is new data comming from write method")
+# fil.close()
+
+
+# fil = open("teswt.txt", "a")
+# fil.write("\nthis is new data..")
+# fil.close()
+
+x = int(input("ashdvjasdvja"))
+
+
+
+
+
