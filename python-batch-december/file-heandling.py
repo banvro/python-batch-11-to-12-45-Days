afedcc63ba607ac24fd47e18d9b9428eb4bc035c
Naresh Kumar <72598101+banvro@users.noreply.github.com>
2024-01-02 21:20:02 -0800

    Create file-heandling.py

---
+++ python-batch-december/file-heandling.py
@@ -0,0 +1,49 @@
+# a = 10
+# b = 20
+
+# c = a + b
+# print(c)
+
+
+# open()
+
+# fil = open("mytxt.txt", "r")
+
+# print(fil.readline())
+# print(fil.readline())
+# # print(fil.read())
+
+# fil.close()
+
+
+# xyz = open("mytxtw.txt", "w")
+# xyz.write("this is new data this is bus")
+# xyz.close()
+
+# fil = open("mytxt.txt", "r")
+# print(fil.read())
+# fil.close()
+
+
+fil = open("mytxti.txt", "a")
+fil.write("\nthis data coe from append mode")
+fil.close()
+
+
+
+a = ?
+
+x.txt 
+
+b = ? 
+
+y.txt 
+
+
+abc = x.xyz
+
+xyz = y.txt 
+
+sum = abc + xyz 
+
+resslt.txt
