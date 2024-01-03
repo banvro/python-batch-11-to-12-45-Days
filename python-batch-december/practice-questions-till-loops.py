91be2fdefaa78ef1eb8fc4670d4ca979c7162887
Naresh Kumar <72598101+banvro@users.noreply.github.com>
2024-01-02 21:13:13 -0800

    Create practice-questions-till-loops.py

---
+++ python-batch-december/practice-questions-till-loops.py
@@ -0,0 +1,38 @@
+question : 1 
+# FizzBuzz Problem: Write a Python program that prints the numbers from 1 to 100. But for multiples of 3, print "Fizz" instead of the number, and for the multiples of 5, print "Buzz." For numbers that are multiples of both 3 and 5, print "FizzBuzz."
+
+question : 2
+# Write a Python function to calculate the factorial of a non-negative integer.
+
+question : 3
+# print series : 
+[1, 2, 333, 4, 55555, 6, 7777777, 8, 999999999, 10]
+
+
+
+
+
+**** Calculator *****
+
+num1 = ?
+num2 = ?
+
+:::::: weleome to my calculator :::::
+    1) Adation
+    2) Subtraion
+    3) Multiplication
+    4) Devsion
+
+choice = ?
+
+
+
+
+
+1) Print ented number by user is devisbile by 2 or not?
+2) Print Passig data to a veriable is integer, float, or string properlry:
+    like:
+        a = 12
+    output : this is integer value
+        a = True
+    output : this is boolean value
