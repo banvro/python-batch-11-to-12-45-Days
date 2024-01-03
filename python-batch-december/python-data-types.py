1725c2ed8a76f4b426932272032ba9c2906c7571
Naresh Kumar <72598101+banvro@users.noreply.github.com>
2024-01-02 21:05:31 -0800

    Create python-data-types.py

---
+++ python-batch-december/python-data-types.py
@@ -0,0 +1,64 @@
+
+
+# Data types:
+#     1) Integer  10, 90, -900
+#     2) Float  10.0, 89.7
+    
+#     3) String
+#         'this is a string'
+#         "this is also a string"
+#         """this is also a string"""
+        
+#     4) Boolean  True   False
+#     5) Complex  a + bj
+#                 10 + 20j
+    
+#     spacial Datatypes:
+#         6) List
+#         7) Tuple
+#         8) Set
+#         9) Dictionry
+
+
+
+# --------------------------------------------
+
+# Comments : to increase code readability
+
+
+# Types of Comment:
+#     1) Single Line Comment
+    
+#     # aksjbdakjsda sdkas dkjasbdkjsadbsajdbsakdjsakda
+    
+#     2) MultiLine Comment
+
+# """
+# ajsdjkasvdkbasd]asdvasjdhvasjd
+# asjdbaskdjasdas
+# daskdvaskdasd
+# adkjabdkasdbjkad
+# asdksavdkbasjkdsa
+# dsakdbl
+# """
+
+# -----------------------------------------
+
+# Functions:
+    
+# # xyz()
+#     types of function:
+#         1) built-in function
+#         2) User Define Function
+
+
+#  1) built-in function:
+#      1) print()
+#      2) input()
+#      3) type()
+#      4) len()
+#      5) count()
+
+a = 120
+
+print("hello word ", a, "this is ok", a, a)
