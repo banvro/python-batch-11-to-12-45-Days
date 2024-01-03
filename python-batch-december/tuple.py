c31b929b1fc960d8542a25306c185b7d9c0bfddd
Naresh Kumar <72598101+banvro@users.noreply.github.com>
2024-01-02 21:18:32 -0800

    Create tuple.py

---
+++ python-batch-december/tuple.py
@@ -0,0 +1,46 @@
+
+
+# List : ordered, allow duplicate values, mutable
+
+# Tuple :
+#     ordered, allow duplicate values, imutable
+    
+#     ()
+
+
+
+# tpl = (10, 20, "hlo", 10.9, True, 10, 20)
+
+# print(tpl)
+
+# print(type(tpl))
+
+# print(tpl[3])
+
+
+
+# tpl = (10, 20, 100, 90, 78, "hlo")
+
+# print(tpl)
+
+# lst = list(tpl)
+
+# print(lst)
+
+# lst.append(200000)
+
+# print(lst)
+
+# newtpl = tuple(lst)
+
+# print(newtpl)
+
+
+
+
+# tpl = (10, 20, "hlo", "car", "bus", 100)
+
+
+
+
+# [10, 20, "hlo", "car", "bus", 100]
