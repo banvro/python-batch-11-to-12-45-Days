3565a998fc96e3b3316500464b72726ffca5e71c
Naresh Kumar <72598101+banvro@users.noreply.github.com>
2024-01-02 21:09:42 -0800

    Create for-loop-or-while-loop.py

---
+++ python-batch-december/for-loop-or-while-loop.py
@@ -0,0 +1,89 @@
+# Loops:
+    
+    
+#     While Loop
+
+# while condation:
+
+# a = 0
+#     #  0 < 10
+# while a < 10:
+#     print(a)
+#     a = a + 1
+    
+
+# for loop : 
+
+# a = "this is a car"
+
+
+# for x in a:
+#     print(x)
+#     print("helo")
+#     print(x, x, "ghety")
+
+
+# print("doneeee")
+
+
+
+
+
+# range(start, end, increment)
+
+# default values:
+#     start = 0
+#     end = n - 1
+#     incmrent = 1
+
+
+# for i in range(3, 12, 2):
+#     print(i)
+
+# 2 x 1 = 2
+# 2 x 2 = 4
+# 2 x 3 = 6
+# ,
+# .
+# .
+# .
+# 2 x 10 = 20
+
+
+# for j in range(1, 11):
+#     print("2 x", j, "=", 2*j)
+
+
+
+# len()
+
+# a = "this is a car"
+
+# # print(len(a))
+# w = 0
+
+# for i in a:
+#     w = w + 1
+
+# print(w)
+
+
+
+# zx = "thisis is this is is sis ss this is car"
+
+# # print(zx.count("i"))
+
+# totle = 0
+
+# for thisisveri in zx:
+#     if thisisveri == "i":
+#         totle = totle + 1
+
+# print(totle)
+
+
+
+
+# xyz = "this is  a car"
+
+# print(xyz[-3])
