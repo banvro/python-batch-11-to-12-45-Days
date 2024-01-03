c410e586c9b19109c0219aca739ffc2a2b3ae346
Naresh Kumar <72598101+banvro@users.noreply.github.com>
2024-01-02 21:14:45 -0800

    Create python-list.py

---
+++ python-batch-december/python-list.py
@@ -0,0 +1,85 @@
+
+# Spacial Data Types:
+#     1) List
+#     2) Tuple
+#     3) Set
+#     4) Dictionry
+
+# 1) List
+
+# a = 10
+
+# 1) Ordered, Allow Duplicate values, Mutable
+
+# []
+
+
+# lst = [12, 10, 100, "hlo", 10.9, "100", 2000]
+
+# print(lst)
+
+# print(type(lst))
+
+
+# index
+
+# lst = [12, 10, 100, "hlo", 10.9, "100", 2000]
+
+# listname[indexnumber]
+
+# print(lst[2])
+
+# print(lst[4])
+
+# print(lst[-1])
+
+
+# Slicing : 
+
+# lst = [12, 10, 100, "hlo", 10.9, "100", 2000]
+
+# print(lst[2])
+
+# listname[start : end : incremnt]
+
+# default values:
+#     start = 0
+#     end = n - 1
+#     increment = 1
+
+# lst = [12, 10, 100, "hlo", 10.9, "100", 2000]
+
+# print(lst[2 : 6 ])
+
+# lst = [12, 10, 100, "hlo", 10.9, "100", 2000]
+
+# # print(lst[3 : ])
+
+
+# print(lst[ : 5])
+
+
+# lst = [12, 10, 100, "hlo", 10.9, "100", 2000]
+
+
+
+# print(lst[: : -1])
+
+
+
+
+1)
+
+a = "thisisacar thisisabus thisisatrain"
+
+
+# output : 
+thisisacar
+thisisabus
+thisisatrain
+
+2)
+
+zx = "this is a good car, and god helps you to find a new good car"
+
+remove : o, a
