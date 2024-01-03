3dc194ae06ceecf9d3b5dd798210a460cfc19ed9
Naresh Kumar <72598101+banvro@users.noreply.github.com>
2024-01-02 21:04:44 -0800

    Update python-intro.py

---
+++ python-batch-december/python-intro.py
@@ -0,0 +1,115 @@
+# Python History : 
+    
+    
+#     1980 
+    
+# 1) ABC language
+# 2) C
+# 3) Objective
+# 4) Unix
+# .
+# .
+# .
+
+# Guido Van Rossum
+
+# 1980 ----- 1991
+#     Python 0.9.0
+
+# 1994 ------ Python 1
+
+# 2000 ------ python 2
+# `               ended (2020)
+
+# 2008 ----- python 3
+
+# Python 3.12.1
+
+
+
+# Unique Features:
+#     1) Simple
+#     2) Gernal Perpose
+#     3) High Level
+#     4) Object Oriented
+#     5) Scripting
+#     6) Interpreated
+#     7) Dynmically typed
+#     8) Case Sensive....
+
+# Simple
+
+
+# # include<stdio.h>
+# # include<conio.h>
+
+# int main(){
+#     int a;
+#     int b, c;
+#     a = 10;
+#     b = 20;
+#     c = a + b;
+#     printf("%d", c);
+# }
+
+
+# a = 10
+# b = 20
+# c = a + b
+# print(c)
+
+# # a = 20
+
+
+# 2) used for verious Pupose:
+
+# 1) Web devlopment
+#     1) Flask
+#     2) Django
+    
+# 2) API Devlopment
+#     DRF (Django Rest framework)
+
+# 3) Desktop Applications
+#     PyQt5
+
+# 4) AI
+
+
+
+# 3) Language:
+#     1) High level language
+    
+    
+#     2) Low level Language/ machine language
+#         binary language
+#         01
+
+
+# 4) Compiler          Interpreater
+
+
+
+# 5) Scripting : 
+
+# python follow top-down approch
+
+
+# a = 10
+# b = 20
+# c = a + b
+# print(c)
+
+
+
+# 8) Case Sensive....
+
+
+# age = 20
+
+# Age = 40
+
+
+# print(Age)
+
+
