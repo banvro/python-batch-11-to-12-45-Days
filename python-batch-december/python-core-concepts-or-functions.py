22be410aa5dba85fb24d4ab9b0d86480f3125c27
Naresh Kumar <72598101+banvro@users.noreply.github.com>
2024-01-02 21:06:38 -0800

    Create python-core-concepts-or-functions.py

---
+++ python-batch-december/python-core-concepts-or-functions.py
@@ -0,0 +1,179 @@
+# PYTHON IDENTIFIERS, KEYWORDS AND INDENTATION
+
+# xyz = 10
+
+# print(xyz)
+
+
+# keywords : reserved words
+#     int, float, print, list, class, def ..... 
+
+
+# int = 10
+
+
+# indentations : 
+
+
+
+
+# if (10 == 10){
+#     print("they are equal")
+# }
+
+# if 10 == 10:
+#     print("they are equal")
+
+# functions:
+#     1) print()
+#     2) input()
+#     3) type()
+#     4) len()
+#     5) count()
+
+# a = 10
+
+# print("hello world ", a, "heyyyy")
+
+# 2) input() : to get input from user
+
+
+
+# age = input("Enter your age : ")
+
+# print("User Enterd : ", age)
+
+
+# input always get input in the doem string
+
+
+# concatenation : joing two strings
+
+# num1 = input("Enter First numver : ")
+# num2 = input("Enter Second Number : ")
+
+# x = num1 + num2
+
+# print("The sum is : ", x)
+
+
+
+
+# Types Casting : 
+
+# a = '10.9'
+# a = "this is also a string"
+# a = """this is also a string"""
+
+
+# type() => it returns what type of data our veriable hold
+
+
+# a = True
+# a = 10 + 78j
+
+# print(type(a))
+    
+
+# a = 10
+
+# print(a, type(a))
+
+# x = float(a)
+    
+# print(x, type(x))
+    
+    
+    
+# age = "this is a car"
+
+# print(age, type(age))    
+
+# xyz = int(age)
+
+# print(xyz, type(xyz))    
+    
+
+
+
+# num1 = int(input("Enter First numver : "))
+# num2 = int(input("Enter Second Number : "))
+
+# x = num1 + num2
+
+# print("The sum is : ", x)
+
+# print("1000" + "7000")
+
+# print("hello" + "world")
+
+# print(1000 + "2000")
+
+
+# Objects : 
+    # 1) Ittrable Objects
+    #     string, list, tuple, set, dectonry
+    
+    # 2) Non ittrable objects
+    #     integer, flot, complex, booelan
+
+
+
+# 1) len() => it returns the number of elements in a perticuler squence.
+
+
+# a = "this is  a ca5r"
+
+# len()
+
+# print(len(a))
+
+# a = 92384932469
+# print(len(a))
+
+
+
+
+
+# count() : it returns a perticler elements how many times apper in a sequnec
+
+
+# st = "this car is a car, this a bus and car this is car car"
+
+# print(st.count("a"))
+
+
+
+1) 
+    
+    a = "200"
+    b = 100
+    
+    output : 300
+
+
+2) 
+    zx = 2938462398463246329438432894
+
+    return how manys time 4 appear
+    
+3)
+    
+    age = int(input : .............)
+    
+    return len of input
+    
+    82937  => 5
+
+
+
+
+# Types casting : 
+
+# a = "1000"
+
+# print(a, type(a))
+
+# d = int(a)
+
+# print(d, type(d))
