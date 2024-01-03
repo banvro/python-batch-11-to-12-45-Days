f82b0d5163d6b5c52ab29e7dad861171317a33d6
Naresh Kumar <72598101+banvro@users.noreply.github.com>
2024-01-02 22:11:56 -0800

    Create start-funcation.py

---
+++ python-batch-december/start-funcation.py
@@ -0,0 +1,112 @@
+# Functions:
+#     1) Buit in
+#     2) user define
+
+# xyz()       code optimization
+
+# DRY
+
+# a = 10
+# b = 20
+# c = a + b
+# print(c)
+
+# q = 120
+# r = 90
+# f = q + r
+# print(f)
+
+# u = 90
+# o = 10
+# er = u + o
+# print(er)
+
+
+
+# print("hello world")
+
+
+# print("hello world")
+
+# print("hello world")
+
+
+# function
+
+# def funcationname():
+#     # block of code
+
+
+# decleartion of funcaion
+
+# def hlo():
+#     print("hello world")
+
+# call a funcation
+
+# hlo()
+
+# hlo()
+
+# hlo()
+
+
+# def sumthis():
+#     a = 10
+#     b = 20
+#     c = a + b
+#     print(c)
+
+# sumthis()
+
+# sumthis()
+
+# perameters or arguments 
+
+
+# def xyz(a, b):
+#     print("helloooooo")
+
+
+# # arguments
+# xyz(10, 20)
+
+
+# type of perameters:
+#     1) Postioanl Perameters
+#     2) Keyword arguments
+#     3) Defult Peramters
+#     4) Veriable Length Peramteters
+#         1) *args
+#         2) **kwargs
+
+
+#  1) Postioanl Perameters
+
+# def xyz(a, b):
+#     print(f"the sum of {a} and {b} is : {a + b}")
+    
+# xyz(12, 10)
+
+# xyz(100, 200)
+
+# xyz(1, 2)
+
+# keyword peramters/arguments
+
+def savedata(a, b):
+    print(f"username is {a} and user age is {b}")
+
+savedata(b = 20, a = "kriss")
+
+
+
+
+
+
+
+
+
+
+
+
