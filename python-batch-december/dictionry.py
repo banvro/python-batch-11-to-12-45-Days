73117bc8c5a72015c85dd824e8f59797c65468e4
Naresh Kumar <72598101+banvro@users.noreply.github.com>
2024-01-02 21:19:25 -0800

    Create dictionry.py

---
+++ python-batch-december/dictionry.py
@@ -0,0 +1,65 @@
+# Dictionry : ordered, do not allow duplicate values, mutable
+
+# JSON
+
+# {key : value, key : value}
+
+
+# di = {"name" : "kriss", "age" : 20, "number" : 234382643}
+
+# print(di["name"])
+
+# print(type(di))
+
+
+# dic = {
+#     "name" : "kriss",
+#     "age" : 20,
+#     "number" : 938468435,
+# }
+
+# to add new data in dictionry:
+
+# dictionryname["newkey"] = "its value"
+
+# dic["Address"] = "this is address"
+
+# to delete ecord
+    # 1) pop()
+    
+# dic.pop("age")
+# dic.pop("number")
+
+# print(dic)
+
+
+
+
+# di = {
+#     1 : {
+#         "Name" : "kriss",
+#         "age" : 20,
+#         "number" : {
+#             "number1" : 98236842,
+#             "number2" : 983264264
+#         }
+#     },
+#     2 : {
+#         "name" : "Moris",
+#         "age" : 90
+#     }
+# }
+
+# print(di[1]["number"]["number2"])
+
+
+# lst = [10, 10, 20, [20, 10, 67, 90], 100, 78]
+
+# print(lst[3][-1])
+
+
+# lst = [10, 20, 10, (12, 100, 78, "hlo"), [10, 78], 100, 78]
+
+# print(lst)
+
+# print(lst[-3][-1])
