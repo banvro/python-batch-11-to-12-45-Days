f994e081dfa2ecd858a3d9b0da7267e23478160f
Naresh Kumar <72598101+banvro@users.noreply.github.com>
2024-01-02 21:14:14 -0800

    Create solution-some-questions.py

---
+++ python-batch-december/solution-some-questions.py
@@ -0,0 +1,56 @@
+# 4
+
+# 4 x 3 x 2 x 1 => 24
+
+# choice = int(input("ENter a  number : "))
+
+# zx = 1
+#     3 > 1
+# while choice > 1:
+#     zx = zx * choice #4
+#     choice = choice - 1
+    
+# print(zx)
+
+
+# cho = int(input("Enter any Number : "))
+
+# zx = ""
+# totle = 1
+
+# for i in range(cho, 0, -1):
+#     if zx == "":
+#         totle = totle * i
+#         zx = zx + str(i)
+#         continue
+#     elif i == 1:
+#         totle = totle * i
+    
+#     else:
+#         zx = zx + " x " + str(i)
+#         totle = totle * i
+
+# print(zx, " = ", totle)
+
+
+# 1
+# 2
+# 333
+# 4
+# 55555
+# 6
+# 777777
+# 8
+# 99999999
+# 10
+
+
+# for i in range(1, 11):
+#     if i % 2 != 0:
+#         print(i * str(i))
+#         continue
+#     print(i)
+
+# print("helo" + "world")
+
+# print(2 * "ra")
