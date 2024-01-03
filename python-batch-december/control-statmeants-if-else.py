8bdd7950995b806561b2043b1f37bc8affb93ce0
Naresh Kumar <72598101+banvro@users.noreply.github.com>
2024-01-02 21:07:26 -0800

    Create control-statmeants-if-else.py

---
+++ python-batch-december/control-statmeants-if-else.py
@@ -0,0 +1,104 @@
+# Control Statments : those statments, which control the flow of execution of a program.
+
+# 1) Condational Statments : 
+    
+# 2) Loops
+
+# 1) Condational Statments : 
+#     if
+#     elif
+#     else
+
+# compersion operators:
+    # ==
+    # !=
+    # >
+    # <
+    # >=
+    # <=
+    
+    
+    
+# print(11 > 10)
+
+
+
+# age = 20
+
+# if condation:
+    # block of code
+
+# age = 30
+
+# if age == 30:
+#     print("these are equal")
+
+# else:
+#     print("these are not equal")
+
+# print("helloooooooo")
+
+
+
+# Voating system : 
+
+# age = int(input("Enter your age : "))
+
+# # 20 > 18
+# if age > 18:
+#     print("you are eligible for voat")
+
+# # elif condation
+# elif age == 18:
+#     print("welcome this is your first voat")
+
+# elif age == 19:
+#     print("this is your second voat")
+
+# else:
+#     print("you are not eligible for voat")
+
+
+
+# Nested if else
+
+
+# age = 17
+
+# if age > 18:
+#     if age == 19:
+#         print("this is 19")
+#     elif age == 18:
+#         print("these are 18")
+
+# elif age ==17:
+#     if age < 17:
+#         print("this s ok")
+
+# else:
+#     print("done")
+
+
+
+
+# print("""
+
+# 1) this is first line
+# 2) this is seond line
+# """)
+
+
+
+
+**** Calculator *****
+
+num1 = ?
+num2 = ?
+
+:::::: weleome to my calculator :::::
+    1) Adation
+    2) Subtraion
+    3) Multiplication
+    4) Devsion
+
+choice = ?
