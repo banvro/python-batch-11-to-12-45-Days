037729b4730bc3b1430745c8fff52d884d0edff8
Naresh Kumar <72598101+banvro@users.noreply.github.com>
2024-01-02 21:08:23 -0800

    Create python-loops-for-loop.py

---
+++ python-batch-december/python-loops-for-loop.py
@@ -0,0 +1,155 @@
+# a = "10"
+
+# if type(a) == int:
+#     print("this is integer value")
+
+# elif type(a) == str: 
+#     print("this is string value")
+
+
+# Loops:
+    # ittrations/ repetation
+    
+    # 1) For Loop
+    # 2) While Loop
+
+# 1) For Loop : 
+
+# st = "this is a car"
+
+
+# for variable in sequnce:
+#     block of code
+
+# for a in st:
+#     print(a)
+#     print("helloooooo")
+#     print(a, a, a, "heloo")
+
+
+# a = 9128312983
+
+# for i in a:
+#     print(i)
+
+
+
+# lst = [10, 20, 10, 7, 98, 10, 45, 30]
+
+# for i in lst:
+#     print(i)
+#     print("heyyyyyyyyyyyyyyyyyyyyyyyyyyy")
+
+
+# range()
+
+
+# range(start, end, increment)
+
+# default values:
+#     start = 0
+#     end = n - 1
+#     increment = 1
+
+
+# for i in range(1, 21, 3):
+#     print(i)
+
+# for j in range(4, 15):
+#     print(j)
+
+
+
+
+# for i in range(1, 11):
+#     print("helooooooo")
+
+
+
+# 2 x 1 = 2
+# 2 x 2 = 4
+# 2 x 3 = 6
+# 2 x 4 = 8
+# ..
+# .
+# .
+# ..
+
+# 2 x 10 = 20
+
+
+# for i in range(1, 11):
+#     # print("2 x", i, "=", 2*i)
+#     print(f"2 x {i} = {2*i}")
+
+
+
+
+
+
+# While Loop : 
+    
+# lst = [2, 1, 4]
+
+# zx = 0
+
+# for i in lst:
+#     zx = zx + i # 2, 3
+
+# print(zx)
+    
+
+
+# Condation based loop:
+
+# while condation:
+#     balock of code
+
+
+# a = 1
+
+#     # 3 < 10
+# while a < 10:
+#     print(a)
+#     a = a + 1
+
+
+# a = 0
+
+# while a < 10:
+#     a = a + 1
+#     print(f"2 x {a} = {2*a}")
+    
+
+# a = 2983469238649
+
+# for i in range(10, 0, -1):
+#     print(i)
+
+
+# a = 298369236493213
+
+# b = str(a)
+
+# totle = 0
+
+# for i in b:
+#     totle = totle + int(i)
+    
+# print(totle)
+
+
+
+
+a = 123
+
+b = str(a)
+
+x = 5
+
+for i in b:
+    x = x + 1
+
+print(x)
+
+# print(len(b))
