a2061d05004d23106cf82ce3dc5f9423390600e9
Naresh Kumar <72598101+banvro@users.noreply.github.com>
2024-01-02 21:16:41 -0800

    Create list-3.py

---
+++ python-batch-december/list-3.py
@@ -0,0 +1,67 @@
+# List : 
+
+# st = "this is a car this this is a bus this is train"
+
+# xyz = ""
+# for i in st:
+#     if i == "a" or i =="i":
+#         continue
+#     xyz = xyz + i
+
+# print(xyz)
+
+# 0 * 0
+# 0 * 1
+# 1 * 0
+# 1 * 1
+
+# st = "this is a car this this is a bus this is train"
+
+# xyz = ""
+
+# a = 0
+#     # 2 < 45
+# while a < len(st):
+#     if st[a] == "i" or st[a] == "a":
+#         a = a + 1
+#         continue
+#     xyz = xyz + st[a] #thi
+#     a = a + 1
+
+
+
+# print(xyz)
+
+
+
+# List: ordeered, allow duplicate values, mutable (chnage)
+
+
+
+
+# lst = [12, 10, 200, "hlo", "10.9", 12, 10]
+
+# to add new data in list:
+    # 1) appned()
+    # 2) insert()
+
+# lst.append(2000)
+# lst.append(100000)
+
+# lst.insert(2, 3000)
+
+# print(lst)
+
+
+lst = [12, 10, 200, "hlo", "10.9", 12, 10]
+
+# to remove data from list:
+#     1) pop()
+#     2) remove()
+
+# lst.pop()
+# lst.pop()
+
+lst.remove(lst[-2])
+
+print(lst)
