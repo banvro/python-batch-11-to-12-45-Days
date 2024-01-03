2dfb18cc9d48e6cee43de26a3046d9df0e8d53f6
Naresh Kumar <72598101+banvro@users.noreply.github.com>
2024-01-02 21:11:48 -0800

    Create break-continue-pass-assert.py

---
+++ python-batch-december/break-continue-pass-assert.py
@@ -0,0 +1,77 @@
+
+# 1) break
+
+
+# zx = "this is this"
+
+# for i in zx:
+#     if i == "s":
+#         break
+#     print(i)
+
+
+
+# for i in range(1, 19):
+#     if i == 8:
+#         print(i)
+#         break
+    
+
+# a = 0
+
+# while a < 10:
+#     a = a + 1
+#     print(a)
+#     if a == 6:
+#         break
+
+
+
+
+# Continue:
+
+# for i in range(1, 19):
+#     if i == 6:
+#         continue
+#     print(i)
+
+
+
+# for i in range(1, 21):
+#     if i % 2 == 0:
+#         continue
+#     print(i)
+
+# print(5 % 2 == 0)
+
+
+# pass
+
+
+# for i in range(1, 10):
+#     if i == 1:
+#         pass
+#     print(i)
+
+
+# for i in range(1, 10):
+#     pass
+
+# for j in range(1, 20):
+#     pass
+
+
+
+
+# assert 
+
+# assert 10 == 11, "here is an error"
+
+
+
+# for loop
+# while loop
+# continue
+# break
+# pass
+# assert
