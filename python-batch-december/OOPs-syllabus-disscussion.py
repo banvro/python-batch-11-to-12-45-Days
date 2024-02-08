b570cfd62386ae17aee5d14454b5253614091701
Naresh Kumar <72598101+banvro@users.noreply.github.com>
2024-02-08 11:37:37 +0530

    Create OOPs-syllabus-disscussion.py

---
+++ python-batch-december/OOPs-syllabus-disscussion.py
@@ -0,0 +1,83 @@
+# OOPs (Object Oriented programming..)
+
+# class or object
+
+# 1) class
+# 2) Objects
+# 3) Methods
+# 4) self
+# 5) constructor
+# 6) Attributes
+# 7) Inheritance:
+#     1) Signle Level Inheritance
+#     2) Multiple Inheritance
+#     3) Multilevel Inheritance
+#     4) Hirirachical Inheritance
+#     5) Hybrid Inheritance
+
+# 8) Abstraction:
+#     1) Abstract Class
+#     2) Abstract Methods 
+#     3) Interfaces
+
+# 9) Ecapsulation:
+#     1) Access Specifiers:
+#         1) Public Mambers
+#         2) Private Mambers
+#         3) Protected Mambers
+
+# 10) Polymorphisum:
+#     1) Compile Time Polymorphisum
+#         1) Mehtod Overloading
+#         2) Operator Overloading
+        
+#     2) Run Time Polymorphiusm
+#         1) Method Overriding
+    
+#     Duck Typing
+
+
+# Class : is a blueprint of an object.
+# Object : is an instance of a class.
+
+# self : self is used for refrencing.
+
+# class myfirstclass:
+#     def mthd1(self):
+#         print("i am a method")
+    
+#     def mthd2(self):
+#         print("i am method 2")
+    
+#     def mthd3(self):
+#         print("i am method 3")
+
+# obj = myfirstclass()
+# obj.mthd2()
+
+
+# class calculator:
+#     def sumthis(self, a, b):
+#         z = a + b
+#         print("the sum is :", z)
+    
+#     def substraction(self, a, b):
+#         z = a - b
+#         print(z)
+
+# xyz = calculator()
+# xyz.sumthis(12, 10)
+
+# xyz.substraction(100, 23)
+
+
+
+
+
+# Attributes:
+
+
+
+
+
+WD    DS   DA    ML    DL     AI
