e4625e7ab76f0eef5e55865afe6e10fb71cc5719
Naresh Kumar <72598101+banvro@users.noreply.github.com>
2024-02-08 11:41:24 +0530

    Create abstraction-polumorphism.py

---
+++ python-batch-december/abstraction-polumorphism.py
@@ -0,0 +1,97 @@
+# Inheritance:
+
+# 2) Abstraction:
+#     1) Abstract Class
+#     2) Abstract Method
+#     3) Interface
+
+
+# abc = abstract base class
+
+# ABC
+# abstractmethod
+
+# from abc import ABC, abstractmethod
+
+# class mycls1(ABC):
+#     @abstractmethod
+#     def faqs(self):
+#         pass
+    
+#     @abstractmethod
+#     def privacypoliy(self):
+#         pass
+    
+#     @abstractmethod
+#     def contactus(self):
+#         pass
+    
+#     def homepage(self):
+#         pass
+
+# class startworking(mycls1):
+#     def faqs(self):
+#         print("this is faq page")
+    
+#     def privacypoliy(self):
+#         print("this is privacy policy page")
+        
+#     def contactus(self):
+#         print("this is ontact us page")
+
+# obj = startworking()
+# obj.contactus()
+
+
+
+
+
+# 2) Polymorphism:
+#     poly  : many
+#     morphism : formds
+
+# when one thing have multiple forms is called Polymorphism.
+
+    
+
+
+# Encapsulation:
+#     Access Spacifires:
+#         1) sjkdfbsdjkfd
+#         sdfsdkf
+    
+# global
+    
+
+
+# def xyz():
+#     x = "heloooo"
+#     # print(x)
+#     return x
+
+
+# d = xyz()
+
+# print(d)
+# print(x)
+
+
+# *
+# * *
+# * * *
+# * * * *
+# * * * * *
+
+# z = 6
+
+# for i in range(1, 6):
+#     print("+" * i, "# " * (z-i))
+    
+# for j in range(6, 1, -1):
+#         print("* " * j)
+
+
+z = 6
+
+for i in range(1, z):
+    print("* " *  i + " o" * (z-i) + "t " * (z-i))
