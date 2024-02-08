8bbfe528d096ccc76f27950c3fc04dfac5b7f762
Naresh Kumar <72598101+banvro@users.noreply.github.com>
2024-02-08 11:44:15 +0530

    Create exception-handeing-2.py

---
+++ python-batch-december/exception-handeing-2.py
@@ -0,0 +1,85 @@
+a = 10
+b = 2
+
+# c = a/b
+# print(c)
+
+# x = int(input("Enter your age : "))
+
+# try:
+#     c = a/b
+#     x = int(input("Enter your age : "))
+
+# except ZeroDivisionError:
+#     print("you never devide anything by zero")
+
+# except ValueError:
+#     print("please enter some vaild data")
+    
+# except Exception as e:
+#     print("Here is an error")
+
+# else:
+#     print("tjhe output is : ", c)
+#     print("user age is  : ", age)    
+    
+    
+# try:
+#     a = 10
+#     b = "asdkjabsd"
+#     c = a + b
+#     print(c)
+
+# except Exception:
+#     print('here is error')
+    
+
+# print("heyyyy")
+    
+    
+# a = 10
+# b = "asdkjabsd"
+# c = a + b
+# print(c)
+
+# print("heyyyy")
+    
+    
+    
+# try:
+
+# except Exception:
+    
+# else:
+    
+# finally:
+    
+    
+# a = 10
+# b = 0
+# c = a / b
+
+# print(c)
+
+# print("helloo")
+    
+# print("ssdfjsf")
+    
+a = 10
+b = 0
+
+try:
+    x = a / b
+
+except Exception as e:
+    print(e)
+
+else:
+    print("iutput is : ", x)
+
+finally:
+    print("exception handeling done...")
+
+print("donnee")
+
+print("dfjbsdfksb")
