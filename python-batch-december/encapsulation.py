92bbe7921ff7a7a79f0898902fa0444a6516ccd8
Naresh Kumar <72598101+banvro@users.noreply.github.com>
2024-02-08 11:41:51 +0530

    Create encapsulation.py

---
+++ python-batch-december/encapsulation.py
@@ -0,0 +1,41 @@
+# Encapsulation : when we wrap multiple things in a sungle unit.
+
+
+# class mycls:
+#     def mthd1(self):
+#         print("thissi methodb 1")
+    
+#     def mthd2(self):
+#         print("this is method 2")
+        
+# Access spacifires:
+#     1) Public mambers
+#     2) nprivate mambes  __
+#     3) protected mambersa _
+
+# class mycls:
+#     def _init_(self):
+#         self.__num1 = 200
+#         self.num2 = 300
+    
+#     def __car(self):
+#         print("this is method car", self.__num1)
+    
+#     def new(self):
+#         obj.__car()
+
+# obj = mycls()
+# # print(obj.__num1)
+# # obj.__car()
+# obj.new()
+
+
+
+# class sbcls(mycls):
+#     def bus(self):
+#         print("num 1 from hild : ", self.__num1)
+# # obj = sbcls()
+# obj.bus()
+
+
+Polymorphism:
