af68f3ddfab4f3f4251d176762b976de7730928d
Naresh <banvro07@gmail.com>
2024-03-04 17:47:20 +0530

    done

--- feb-web-devlopment/django/xyz-abc/Hello/Hello/urls.py
+++ feb-web-devlopment/django/xyz-abc/Hello/Hello/urls.py
@@ -22,4 +22,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("blog.urls")),
-] + static(settings.STATIC_URL, documnet_root = settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+ 
