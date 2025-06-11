bad9a4240a0920776b3a2b790ecdc07525ef4e82
Naresh Kumar <72598101+banvro@users.noreply.github.com>
2025-06-11 12:30:16 +0530

    Add files via upload

---
+++ 45 days 11 june 2025/3rd class condational statments.py
@@ -0,0 +1,262 @@
+# Python
+# History
+# Veriables
+# Data Types
+# Type casting
+# Core Functions:
+#     print()
+#     len()
+#     input()
+#     type()
+#     count()
+    
+  
+  
+#   Control Statments:
+#     1) Condational Statments
+#     2) Loops
+    
+    
+# 1) Condational Statments
+#     if, elif, else
+    
+    
+
+# Operator
+#     ---> Compersion Operator
+#         1) ==
+#         2) !=
+#         3) <
+#         4) >
+#         5) <=
+#         6) >=
+
+# ----> True, False
+    
+    
+# print(10 == 11)  
+
+# print(1 != 3)
+
+# print(10 < 10)
+
+# print("10" == 10)
+    
+    
+# syntax:
+
+# spaces ----> indentation
+
+
+# if condation:
+#     # code...
+# else:
+#     # code.....
+    
+
+# x = 9
+
+
+#     # 11 < 10
+# if x < 10:
+#     print("x is small")
+
+# else:
+#     print("x is big")
+    
+    
+# Voatting system
+
+# print("::::Welem to my Voatting System::::\n")
+    
+
+# age = int(input("Enter your age : "))
+
+#     # "20" > 18
+
+# if age > 18:
+#     print("you are eligible for voat..")
+
+# else:
+#     print("you are not eligible for vaot..")
+    
+ 
+# elif
+#     ---> use when we have more then one condation...
+    
+  
+
+# age = int(input("Enter your age : "))
+
+# if age > 18:
+#     print("you are eligble for vaot..")
+
+# elif age == 18:
+#     print("welcme this is your first voat")
+
+# elif age == 19:
+#     print("this is your seocnd voat")
+
+# else:
+#     print("You are not eligible for vaot")
+  
+  
+  
+  
+#  Calculator:
+
+
+# :::::weleme to my calculator::::::
+    
+# : Enter your inputs :
+    
+# num1 = ? 10
+# num2 =  ? 20
+
+# :::Your choices are :::
+    
+#     1) Adation
+#     2) Subtation
+#     3) Multiplication
+#     4) Devsion
+
+# choie = ? 2
+  
+  
+  
+  
+  
+  
+  
+  
+# print("::Welcome to my Calculator:::\n")
+
+# print(":::Enter your inputs:::\n")
+ 
+# num1 = int(input("Enter first number : "))
+# num2 = int(input("Enter second number : "))
+    
+# print("""
+#     Enter Your Choices:::
+#         1) Adation
+#         2) Subtration
+#         3) Multiplication
+#         4) Devsion
+# """)
+ 
+# choice = int(input("Enter your choice : "))  
+# if choice == 1:
+#     zx = num1 + num2
+#     print("the sum is : ", zx)
+
+# elif choice == 2:
+#     zx = num1 - num2
+#     print("the subtraction is : ", zx)
+
+# elif choice == 3:
+#     x = num1 * num2
+#     print("the multiplication is : ", x)
+
+# elif choice == 4:
+#     q = num1 / num2
+#     print("the devsion is : ", q)
+
+# else:
+#     print("Enter vaild key..!")
+    
+    
+# ----> nested if else <---------   
+    
+    
+# age = int(input("Enter your age : "))
+
+# if age >= 18:
+#     if age == 18:
+#         print("this is your first voat")
+    
+#     else:
+#         print("you are eligible for vaot")
+
+# elif age == 17:
+#     if age == 20:
+#         print("helooooooooooooo")
+
+# else:
+#     print("nothing...")
+    
+    
+    
+    
+    
+    # Operators
+    
+# 10 + 20
+
+# operands ---> 10, 20
+
+# opeators ---> +
+    
+    
+# 1) Arithmatical Operaters:
+#     +
+#     -
+#     /
+#     *
+#     % --> Modules
+#     // --> floor devsion
+    
+# print("10" / 20)
+    
+# print("Pyhon " * 100)
+
+# print(10 % 2)
+
+# print(5 % 2)
+
+
+
+# problem : take a input from user and print the enterd numver is even or odd.
+
+
+
+# num = int(input("Enter a number : "))
+
+# if num % 2 == 0:
+#     print("the number is even")
+
+# else:
+#     print("the number is odd")
+
+
+
+# 3) Mambership Operators:
+#     1) in 
+#     2) not in
+
+
+# x = "this is a car"
+    
+# print("bus" in x)
+    
+    
+    
+zx = "this is a car and this is train"
+
+if "plan" not in zx: 
+    print("data exist")
+
+else:
+    print("data is not ecist")
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+    
+    
+    
+    
