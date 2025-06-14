4be90fdf611a7f825239a8b6f5dcd6e25193e82c
Naresh Kumar <72598101+banvro@users.noreply.github.com>
2025-06-14 13:40:52 +0530

    Create Lecture 5 - While Loop & control statements.py

---
+++ 45 days 11 june 2025/Lecture 5 - While Loop & control statements.py
@@ -0,0 +1,363 @@
+# Loops:
+#     for loop
+
+# range()
+
+# 10
+# 9
+# 8
+# 7
+# 6
+# 5
+# 4
+# 3
+# 2
+# 1
+
+
+# for i in range(10, 0, -1):
+#     print(i)
+    
+    
+#   ----------------------------------
+
+# while loop:--
+
+# for loop ---> sequence
+
+# while loop ---> condation
+    
+
+# Syntax:
+    
+# while condation:
+#     # code..
+    
+    
+# while 10 == 10:
+#     print("helloooooo")
+  
+  
+  
+# genrate 1 to 10 using while loop
+
+
+# a = 1
+
+# while a < 10:
+    
+#     print(a)
+    
+#     a = a + 1 # 3
+
+
+
+# 1
+# 2
+# 3 3 3
+# 4
+# 5 5 5 5 5
+# 6
+# 7 7 7 7 7 7 7
+# 8
+# 9 9 9 9 9 9 9 9 9
+# 10
+
+
+# a = 1
+
+# while a < 11:
+    
+#     if a % 2 == 0:
+#         print(a)
+#     else:
+#         print(str(a) * a)
+
+#     a = a + 1
+
+# print("heloo" * 5)
+
+# febnoci series
+
+# 0
+# 1
+# 1
+# 2
+# 3
+# 5
+# 8
+# 13
+# 21
+# 34
+# 55
+
+
+
+# Control statments:
+#     1) break
+#     2) continue
+#     3) pass
+
+    
+#  break ---->
+  
+  
+# a = "12303240"
+
+# for i in a:
+
+#     if i == "0":
+#         break
+    
+#     print(i)
+    
+    
+# zx = "1298302002302070233"
+    
+    
+# for i in zx:
+#     if i == "7":
+#         break
+    
+#     print(i)
+    
+    
+
+# 2) continue:
+
+
+# x = "12037103020100000234003"
+
+# for i in x:
+    
+#     if i == "0":
+#         continue
+    
+#     print(i)
+
+
+# 1
+# 2
+# 4
+# 6
+# 7
+# 8
+# 10
+
+
+# Operators:
+
+# logical operators
+# and
+# or
+# not
+
+
+# and
+
+# 1   1  |  1
+# 1   0  |  0
+# 0   1  |  0
+# 0   0  |  0
+
+
+# if 10 != 10 and 10 != 11:
+#     print("doneeeee")
+
+    
+# or
+
+# 1    1   1
+# 1    0   1
+# 0    1   1
+# 0    0   0
+    
+ 
+# if 10 == 10 or 2 > 10 or 2 == 3:
+#     print("okkkkkkkk")
+   
+
+# 1
+# 2
+# 4
+# 6
+# 7
+# 8
+# 10
+   
+#   3, 5, 9
+# x = 1
+
+# while x < 11:
+    
+#     if x == 3 or x == 5 or x == 9:
+#         x = x + 1
+#         continue
+    
+#     print(x)
+    
+#     x = x + 1
+   
+
+# x = "359" 
+
+# for i in range(1, 11):
+    
+#     if str(i) in x:
+#         continue
+    
+#     print(i)
+
+
+# 3) pass
+    # ---> 
+    
+    
+# for i in range(1, 100):
+#     pass
+
+
+
+# loops --> for loop, while loop
+
+# while True
+
+
+
+# while loop   |  while True
+
+
+
+# while condation:
+#     code....
+
+
+
+# while True:
+#     # if condation...
+#         # code
+
+
+# print(" :::: welcome to number adding loop::::\n")
+
+# while True:
+    
+#     choice = input("Press q for quit, a for add numbers and m for pettrn : ")
+    
+#     if choice == "q":
+#         break
+    
+#     elif choice == "a":
+#         num1 = int(input("Enter first number : "))
+#         num2 = int(input("Enter second number : "))
+        
+#         zx = num1 + num2
+#         print(f"The sum is : {zx}")
+    
+#     elif choice == "m":
+#         inp = int(input("Enter the number for pattrn : "))
+        
+#         for i in range(1, inp + 1):
+#             print(i * f"{i} ")
+#     else:
+#         print("Enter valid key \n")
+            
+
+
+
+
+# indexing::::::::::
+
+# string, list, tuple
+
+# x = "this is a car"
+
+# q = [12, 34, 45, 56, 67, 8, 100]
+
+
+# x = "hello"
+
+# location ---> address ---> index number
+
+# syntax
+
+# veriable_name[index_number]
+
+# print(x[1])
+
+
+
+# zx = "this is car"
+
+# print(zx[2])
+
+
+# postive indexing
+
+# 0, 1, 2,3 , .....
+
+
+# negtive indexing
+
+# zx = "hey"
+
+# print(zx[2])
+
+# print(zx[-1])
+
+
+
+xy = "hello world"
+
+# for i in x:
+#     print(i)
+
+len_x = len(xy)
+
+x = 0
+
+while x < len_x:
+    
+    print(xy[x])
+    
+    x = x + 1
+
+
+
+
+   
+# Data Structers
+
+
+# 0
+# 1
+# 1
+# 2
+# 3
+# 5
+# 8
+# 13
+# 21
+# 34
+# 55
+
+a = 0
+b = 1
+
+while a < 100:
+    print(a)
+    c = a + b
+    a = b
+    b = c
+
+
+
+
+
+
+
+
+
+
+    
+
