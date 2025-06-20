c146ea861e8615e4dd719a5c099bd93bdc2c8704
Naresh Kumar <72598101+banvro@users.noreply.github.com>
2025-06-20 14:36:15 +0530

    Create lecture-10-:Start-oops.py

---
+++ 45 days 11 june 2025/lecture-10-:Start-oops.py
@@ -0,0 +1,145 @@
+# # Functions:
+    
+# # OOPs
+# # Exception handeling
+# # Reguler expression
+# # Database
+# # tkinter
+ 
+ 
+ 
+#  OOPs
+#     --> object oriented programming..
+    
+# Language 
+#     ---> oops
+#     ------> concept
+#             ----> code
+#             in manged way.
+
+# 10 --- 15
+
+# 10k
+
+# OOP's
+ 
+ 
+# class ---------- object
+
+# Topics:
+#     1) Class
+#     2) Object
+#     3) self
+#     4) Constructer
+#     5) Methods
+#     6)Inheritances:
+#         1) Single Level inheritance
+#         2) Multiple Inheritances
+#         3) Multilevel Inheritances
+#         4) Hararchical Inheritances
+#         5) Hybrid Inheritances
+#     7) Abstraction
+#         1) Abstreact class
+#         2) Abstract Methods
+#         3) Interfaces
+#     8) Encapsulation:
+#             Access Spacifires
+#                 1) Public Mambers
+#                 2) Protected Mambers
+#                 3) Private mambers
+#     9) Polymorphism
+#         1) Compile Time Polymorphism
+#             1) Method overloading
+#             2) Operator overloading
+        
+#         2) Run time Polymorphism
+#             1) Method overriding
+
+
+# Class : is a blueprint of an object.
+
+# Object : is an instance of a class.
+
+
+# class mycls:
+#     code.......
+ 
+ 
+#  code ----> methods
+#         ---> function
+ 
+ 
+#  self---->
+ 
+# class vehcile:
+#     def oddi(self):
+#          print("this is oddi...")
+         
+#     def tesla(self):
+#         print("this is tesla")
+    
+#     def bmw(self):
+#         print("this is bmw")
+    
+#     def maruti(self):
+#         print("this is maruti")
+    
+# obj = vehcile()
+# obj.tesla()
+ 
+ 
+# class
+# object
+# self ----> for refrencing
+# methods
+
+# class calculator:
+#     def summ(self, a, b):
+#         zx = a + b
+#         print(zx)
+    
+#     def subtract(self, a, b):
+#         zx = a - b
+#         print(zx)
+    
+#     def multi(self, a, b):
+#         zx = a * b
+#         print(zx)
+        
+# obj = calculator()
+# obj.subtract(20, 10)
+# obj.multi(40, 3)
+ 
+ 
+
+# Constructor:
+#     ---> is a spacial method in your class.
+#     we buid constructer using __init__ keyword.
+#     the main functionality of constructer is, it calls automatically when we craete an object of c class.
+ 
+ 
+class xyz:
+    def __init__(self):
+        print("this is our constructer")
+    
+    def car(self):
+        print("this is a car")
+    
+    def bus(self):
+        print("this is a bus...")
+ 
+abc = xyz()
+abc.bus()
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+    
