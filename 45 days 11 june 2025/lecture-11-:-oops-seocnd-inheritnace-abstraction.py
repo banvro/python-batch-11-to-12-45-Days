19b63ad990f9daebb5f137aedd70699057ae53ad
Naresh Kumar <72598101+banvro@users.noreply.github.com>
2025-06-23 16:13:00 +0530

    Create lecture-11-:-oops-seocnd-inheritnace-abstraction.py

---
+++ 45 days 11 june 2025/lecture-11-:-oops-seocnd-inheritnace-abstraction.py
@@ -0,0 +1,323 @@
+    # # OOps
+    
+    # # Class
+    # # object
+    # # self
+    # # methods
+    # # constructor
+    
+    
+    
+    # # constructor :
+    # #         --> 
+    
+    # class cls1:
+    #     def __init__(self):
+    #         print("this is a constructer")
+        
+    #     def bus(self):
+    #         print("this is a buss")
+        
+    #     def car(self):
+    #         print("this is a car")
+    
+    # obj = cls1()
+    # obj.car()
+    
+    
+    # -----> attributes
+    
+    
+    
+    # class xyz:
+    #     def __init__(self):
+    #         print("this is a constructer")
+        
+    #     def summ(self, a, b):
+    #         print(a + b)
+    
+    #     def subb(self):
+    #         print(a - b)
+    
+    # obj = xyz()
+    # obj.summ(12, 20)
+    # obj.subb()
+    
+    
+    
+    
+    
+    # class xyzz:
+    #     def __init__(self, x, y):
+    #         self.num1 = x
+    #         self.num2 = y
+        
+    #     def summ(self):
+    #         print(self.num1 + self.num2)
+        
+    #     def subb(self):
+    #         print("this is sebbbb")
+        
+    #     def multi(self):
+    #         print(self.num2 * self.num1)
+    
+    # obj = xyzz(20, 40)
+    # obj.summ()
+    # obj.multi()
+    
+    
+    # class xyzz:
+    #     def __init__(self):
+    #         zx = int(input("Enter first number : "))
+    #         pq = int(input("Enter second number : "))
+    #         self.num1 = zx
+    #         self.num2 = pq
+        
+    #     def summ(self):
+    #         print(self.num1 + self.num2)
+        
+    #     def subb(self):
+    #         print("this is sebbbb")
+        
+    #     def multi(self):
+    #         print(self.num2 * self.num1)
+    
+    # obj = xyzz()
+    # obj.summ()
+    # obj.multi()
+    
+    
+    
+    
+    # Inheritance:
+    #     --> help to access the properties, attributes, methods from one to another.
+    #     relationship
+    #         --> parent child
+    #         child
+            
+    #         parent
+    
+    # Types of inheritnace:
+    #     single level inheitnace
+    #         ---> one parent have one child
+    
+    
+    # class cls1:
+    #     def mthd1(self):
+    #         print("this si elmhod 1")
+        
+    #     def mthd2(self):
+    #         print("this is method 2")
+    
+    # class cls2(cls1):
+    #     def mthd3(self):
+    #         print("this is emthod 3")
+    
+    # obj = cls2()
+    # obj.mthd1()
+    
+    
+    
+    
+    # 2) Multiple Inheritnace
+    #     --> 1 child have multiple parent
+    
+    
+    # class cls1:
+    #     def mthd1(self):
+    #         print("this is method 1")
+        
+    #     def mthd2(self):
+    #         print("this is method 2")
+    
+    # class cls2:
+    #     def mthd3(self):
+    #         print("this is metjod 3")
+    
+    # class cls3(cls1, cls2):
+    #     def mthd4(self):
+    #         print("this is method 4")
+    
+    # obj = cls3()
+    # obj.mthd3()
+    
+    #  1)   single level inheitnace
+    #         ---> one parent have one child
+            
+    # 2) Multiple Inheritnace
+    #     --> 1 child have multiple parent
+    
+    # 3) Multilevel Inheritance
+    #     lavels
+    #     grandparent <--- parent <---- child
+    #     chain
+    
+    # class cls1:
+    #     def mthd1(self):
+    #         print("this is method 1")
+        
+    #     def mthd2(self):
+    #         print("this is method 2")
+    
+    # class cls2(cls1):
+    #     def mthd3(self):
+    #         print("this is metjod 3")
+    
+    # class cls3(cls2):
+    #     def mthd4(self):
+    #         print("this is method 4")
+    
+    # obj = cls3()
+    # obj.mthd1()
+    
+    
+    
+    #  1)   single level inheitnace
+    #         ---> one parent have one child
+            
+    # 2) Multiple Inheritnace
+    #     --> 1 child have multiple parent
+    
+    # 3) Multilevel Inheritance
+    #     lavels
+    #     grandparent <--- parent <---- child
+    #     chain
+        
+    # 4) Hararchical Inheritance
+    #     --> 1 parent have multiple child
+        
+    
+    
+    # class cls1:
+    #     def mthd1(self):
+    #         print("this is method 1")
+        
+    #     def mthd2(self):
+    #         print("this is method 2")
+    
+    # class cls2(cls1):
+    #     def mthd3(self):
+    #         print("this is metjod 3")
+    
+    # class cls3(cls1):
+    #     def mthd4(self):
+    #         print("this is method 4")
+            
+    # obj = cls2()
+    # obj.mthd3()
+            
+    
+    
+    
+    #  1)   single level inheitnace
+    #         ---> one parent have one child
+            
+    # 2) Multiple Inheritnace
+    #     --> 1 child have multiple parent
+    
+    # 3) Multilevel Inheritance
+    #     lavels
+    #     grandparent <--- parent <---- child
+    #     chain
+        
+    # 4) Hararchical Inheritance
+    #     --> 1 parent have multiple child
+        
+    # 5) Haybrid Inheritance
+    #     ---> when we combine 2 or more 2 inheritances. that new inheritnace is called Haybrid Inheritance.
+        
+        # Hararchical + Multiple
+    
+    
+    # class cls1:
+    #     def mthd1(self):
+    #         print("this is method 1")
+        
+    #     def mthd2(self):
+    #         print("this is method 2")
+    
+    # class cls2(cls1):
+    #     def mthd3(self):
+    #         print("this is metjod 3")
+    
+    # class cls3(cls1):
+    #     def mthd4(self):
+    #         print("this is method 4")
+    
+    # class cls4(cls2, cls3):
+    #     def mthd5(self):
+    #         print("this is method 5")
+    
+    # obj = cls4()
+    # obj.mthd4()
+    
+    
+    
+    # -----------------
+    
+    # Abstraction
+    # Encapsulation
+    # Ploymorphism
+    # ---->
+    
+    # projects
+    
+    
+    
+    
+    # Abstraction
+    #     ---> Help us to maintain structure / blueprint
+    
+    
+    
+    # website:
+    #     Contact us
+    #     about us
+    #     privacy policy
+    #     faq
+    #     services
+    #     home
+        
+    
+    # 1) Abstract Classes
+    # Abstract Methods
+    
+    
+    from abc import ABC, abstractmethod
+    
+    class vechical(ABC):
+        @abstractmethod
+        def tyers(self):
+            pass
+        
+        @abstractmethod
+        def body(self):
+            pass
+    
+        @abstractmethod
+        def engine(self):
+            pass
+    
+    class oddi(vechical):
+        def tyers(self):
+            print("these are tyers")
+        
+        def body(self):
+            print("this is body")
+        
+        def engine(self):
+            print("this is an engine...")
+            
+    class tesla(vechical):
+        pass
+        
+    
+    obj1 = tesla()
+    obj = oddi()
+    
+    
+    
+    
+    
+    
+    
