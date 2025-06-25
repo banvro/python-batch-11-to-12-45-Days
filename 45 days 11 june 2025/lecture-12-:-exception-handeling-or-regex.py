5691bd48d6b4de03e6ba2d51161f25e7bc15702b
Naresh Kumar <72598101+banvro@users.noreply.github.com>
2025-06-25 12:02:18 +0530

    Create lecture-12-:-exception-handeling-or-regex.py

---
+++ 45 days 11 june 2025/lecture-12-:-exception-handeling-or-regex.py
@@ -0,0 +1,223 @@
+# OOps:
+
+# Exception Handeling:
+    
+# Coding---> 
+
+# Errors
+
+
+# print("hellooooo")
+
+# print("continueee code....")
+
+# if x == 10:
+#     print("heloooooooo")
+
+# print("conitnueeee")
+
+# print("important codee")
+
+# print("donee")
+
+
+# Types of error:
+#     1) Compile Time error
+#     2) Logical error
+#     3) Run time error
+
+
+# 1) Compile Time error
+#         ---> syntax error
+
+
+# if 10 == 10
+#     print("helloooooo....")
+
+
+# a = 10
+
+# b   20
+
+# c = a + b
+
+# print(c)
+
+
+
+# 2) Logical error:
+
+# a = 10
+
+# b = 0
+
+# c = a / b
+
+# print(c)
+
+
+# 3) Run time error:
+
+
+# age = int(input("Enter your age : "))
+
+# print(age)
+
+
+# Exception Handeling:
+
+# block ----> code --> blocks
+
+
+
+# try:
+#     # doutable code..
+
+# except Exception:
+#     # try have an error
+
+# else:
+#     # work only if try havn't any errir
+
+# finally:
+#     # indicate exception handeling done
+
+
+# print("heloooooooo")
+
+# print("okkkkkkkkkkkkk")
+
+# try:
+#     age = int(input("Enter your age : "))
+
+# except Exception:
+#     print("error occure")
+
+# else:
+#     print(age)
+
+# finally:
+#     print("excetion handeling donee...")
+
+# print("i am inportant code...")
+
+# print("workinnng")
+
+# print("doneee...")
+
+
+
+
+
+# Regex 
+#     ----> reguler expressions
+    
+    
+# 100000 --> text
+#     ----> emails
+
+# text data --->
+
+# pattren ---> regex
+
+
+
+
+
+# import re
+
+# text = """Python is dynamically type-checked and garbage-collected. It supports multiple programming paradigms, including structured (particularly procedural), object-oriented and functional programming. It is often described as a "batteries included" language due to its comprehensive standard library.[34][35]
+
+# Guido van Rossum began working on Python in the late 1980s as a successor to the ABC programming language, and he first released it in 1991 as Python 0.9.0.[36] Python 2.0 was released in 2000. Python 3.0, released in 2008, was a major revision not completely backward-compatible with earlier versions. Python 2.7.18, released in 2020, was the last release of Python 2.[37]
+
+# Python consistently ranks as one of the most popular programming languages, and it has gained widespread use in the machine learning community.[38][39][40][41]
+
+# History
+# Main article: History of Python
+
+# The designer of Python, Guido van Rossum, at PyCon US 2024
+# Python was conceived in the late 1980s[42] by Guido van Rossum at Centrum Wiskunde & Informatica (CWI) in the Netherlands; it was conceived as a successor to the ABC programming language, which was inspired by SETL,[43] capable of exception handling and interfacing with the Amoeba operating system.[12] Python implementation began in December, 1989.[44] Van Rossum assumed sole responsibility for the project, as the lead developer, until 12 July 2018, when he announced his "permanent vacation" from responsibilities as Python's "benevolent dictator for life" (BDFL); this title was bestowed on him by the Python community to reflect his long-term commitment as the project's chief decision-maker.[45] (He has since come out of retirement and is self-titled "BDFL-emeritus".) In January, 2019, active Python core developers elected a five-member Steering Council to lead the project.[46][47]
+
+# The name Python is said to derive from the British comedy series Monty Python's Flying Circus.[48]
+
+# Python 2.0 was released on 16 October 2000, with many major new features such as list comprehensions, cycle-detecting garbage collection, reference counting, and Unicode support.[49] Python 2.7's end-of-life was initially set for 2015, and then postponed to 2020 out of concern that a large body of existing code could not easily be forward-ported to Python 3.[50][51] It no longer receives security patches or updates.[52][53] While Python 2.7 and older versions are officially unsupported, a different unofficial Python implementation, PyPy, continues to support Python 2, i.e., "2.7.18+" (plus 3.10), with the plus signifying (at least some) "backported security updates".[54]
+
+# Python 3.0 was released on 3 December 2008, with some new semantics and changed syntax. At least every Python release since (the now unsupported) 3.5 has added some syntax to the language; a few later releases have removed outdated modules and have changed semantics, at least in a minor way.
+
+# As of 8 April 2025, Python 3.13.3 is the latest stable release (it's highly recommended to upgrade to it, or upgrade any other older 3.x release). This version currently receives full bug-fix and security updates, while Python 3.12—released in October 2023—had active bug-fix support only until April 2025, and since then only security fixes. Python 3.9[55] is the oldest supported version of Python (albeit in the 'security support' phase), because Python 3.8 has become an end-of-life product.[56][57] Starting with Python 3.13, it and later versions receive two years of full support (which has increased from one and a half years), followed by three years of security support; this is the same total duration of support as previously."""
+
+
+# ptrn = r"\d{4}"
+
+# xyz = re.findall(ptrn, text)
+
+# print(xyz)
+
+# [(]\d{3}[)][-]\d{4}[-]\d{3}
+
+# [6789]\d{9}
+
+
+
+
+
+# import re
+
+# data = """Python is dynamically type-checked and garbage-collected. It supports multiple programming paradigms, including structured (particularly procedural), object-oriented and functional programming. It is often described as a "batteries included" language due to its comprehensive standard 9872536478 library.[34][35]
+
+# Guido van Rossum began working on Python in the late 1980s as a successor to the ABC programming language, and he first released it in 1991 as Python 0.9.0.[36] Python 2.0 was released in 2000. Python 3.0, released in 2008, was a major revision not completely backward-compatible with earlier versions. Python 2.7.18, released in 2020, was the last release of Python 2.[37]
+
+# Python consistently ranks as one of the most popular programming languages, 1234567890 and it has gained widespread use in the machine learning community.[38][39][40][41]
+
+
+# History
+# Main article: History of Python
+
+# The designer of Python, Guido van Rossum, at PyCon US 2024
+# Python was conceived in the late 1980s[42] by Guido van Rossum at Centrum xyz@gmail.com Wiskunde & Informatica (CWI) in the Netherlands; it was conceived as a successor to the ABC programming language, which was inspired by SETL,[43] capable of exception handling and interfacing with the Amoeba operating system.[12] Python implementation 6273846527 began in December, 1989.[44] Van Rossum assumed xyz12ab@gmail.com sole responsibility for the project, as the lead developer, until 12 July 2018, when he announced his "permanent vacation" from responsibilities (123)-2332-345 as Python's "benevolent dictator for life" (BDFL); this title was bestowed on him by the Python community to reflect his long-term commitment as the project's chief decision-maker.[45] (He has since come out of retirement and is self-titled "BDFL-emeritus".) In January, 2019, active Python core developers elected a123x123@gmail.com a five-member Steering Council to lead the project.[46][47]
+
+# The name Python is said to derive from the British comedy series Monty Python's Flying Circus.[48]
+
+# Python 2.0 was released on 16 October 2000, with many major new features such as (823)-3485-9273 list comprehensions, cycle-detecting garbage collection, reference counting, and Unicode support.[49] Python 2.7's end-of-life 9182736542 was initially set for 2015, and then postponed to 2020 out of concern that a large body of existing code xyz1@yahoo.com could not easily be forward-ported to Python 3.[50][51] It no longer receives security patches or updates.[52][53] While Python 2.7 and older versions are officially unsupported, a different unofficial Python implementation, PyPy, continues to support Python 2, i.e., "2.7.18+" (plus 3.10), with the plus signifying (at least some) "backported security updates".[54]
+
+# Python 3.0 was 77273745617 pqr11abc@gmail.com released on 3 December 2008, with some new semantics (103)-2343-232 and changed syntax. At least every Python release since (the now unsupported) 3.5 has added some syntax to the language; a few later releases have removed outdated modules and have changed semantics, at least in a minor way.
+
+# As of 8 3452617384 April 2025, Python 3.13.3 is the latest stable release (it's highly recommended to upgrade to it, or upgrade any other older 3.x release). This version currently receives full bug-fix and security updates, while Python 3.12—released in October 2023—had active bug-fix support only until April 2025, and since then only security fixes. Python 3.9[55] is the oldest supported version of Python (albeit in the 'security support' phase), because Python 3.8 has become an end-of-life product. 9182736253 (283)-2343-287 [56][57] Starting with Python 3.13, it and later versions receive two years of full support (which has increased from one and a half years), followed by three years of security support; this is the same total duration of support as previously."""
+
+# ptrn = r"[a-z]+[0-9]*[a-z]*[@][a-z]{5}[.][a-z]{3}"
+
+
+# x = re.findall(ptrn, data)
+
+# print(x)
+
+
+import re
+
+data = "this is car ad this is a good car and car run fast."
+
+ptrn = "car"
+
+xy = re.sub(ptrn, "bus", data)
+
+print(xy)
+
+
+find()
+finditr()
+findall()
+match()
+
+
+
+Tkinter--<------projects
+
+
+
+
+
+
+
+
+
+
