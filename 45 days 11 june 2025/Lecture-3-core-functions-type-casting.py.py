e8ef529aa60bce566deb15d566ecc352dcd168fb
Naresh Kumar <72598101+banvro@users.noreply.github.com>
2025-07-26 14:05:29 +0530

    Add files via upload

---
+++ 45 days 11 june 2025/Lecture-3-core-functions-type-casting.py.py
@@ -0,0 +1,207 @@
+# Core functions:
+#     fucntion 
+
+# 1) print()
+# 2) input()
+# 3) len()
+# 4) type()
+# 5) count()
+
+# 1) print()
+#     --> help us to print anything in terminal.
+
+
+# Data types:
+    
+#             objects
+
+# 1) ittrabe object
+#     1) string
+#     2) list
+#     3) tuple
+#     4) set
+#     5) dictionry
+
+# 2) Non Ittrable objects
+#     1) Integer
+#     2) float
+#     3) boolen
+#     4) complex
+
+
+
+#  1) print()
+
+
+# print("Hello Python")
+
+# x = 1000
+
+# print(x)
+
+
+# x = 1000
+
+# print("the value of x is ", x)
+
+
+# x = 10
+
+# y = 20
+
+# z = x + y
+
+# print("the sum of", x, "and", y, "is :", z)
+
+
+
+# adding two numbers
+
+# x = 20
+# y = 45
+
+# z = x + y
+
+# print(z)
+
+
+# 2) type()
+#     ---> tell us what type of data a veriable hold..
+
+
+# zx = 10 + 20j
+
+# print(type(zx))
+
+
+# 3) input()
+#     ---> to get input from user...
+
+
+
+# name = input("Enter your name : ")
+
+# print("Good Morning,", name)
+
+# Adding two numbers
+
+# num1 = input("Enter first number : ")
+# num2 = input("Enter second number : ")
+
+# zx = num1 + num2
+
+# print("The sum is : ", zx)
+
+
+# input() ---> string
+
+
+# x = "10"
+# y = "2"
+
+# zx = x + y
+
+# print(zx)
+
+
+# concatenation --> joining two strings
+
+
+# Type Casting:
+#     is a process to convert data from one data type to another..
+
+
+# x = "100"
+# print(x, type(x))
+
+# y = int(x)
+# print(y, type(y))
+
+
+
+# a = 10
+
+# print(a, type(a))
+
+# b = float(a)
+# print(b, type(b))
+
+
+# str()   ---> to string
+# float() -----> float
+# int() -----> to integer
+# bool() ----> boolean
+
+
+
+
+# zx = "this is a car"
+# print(zx, type(zx))
+
+
+# p = int(zx)
+
+# print(p, type(p))
+
+
+# x = int(input("Enter first number : "))
+# y = int(input("Enter second number : "))
+
+# z = x + y
+
+# print(z)
+
+
+
+# x = int(input("Enter first number : "))
+# y = int(input("Enter second number : "))
+
+# z = x + y
+
+# print("the sum of", x, "and", y, "is :", z)
+
+
+# print(f"the sum of {x} and {y} is : {z}")
+
+
+# ------------------------------
+
+# count()
+# len()
+
+
+# practice question
+# 1)
+
+x = 10
+y = "20"
+
+# do summm
+
+# 2) output ----> 108
+
+# # input 
+# x = 10
+
+# user inpit xx
+# "1293"   xx
+
+
+
+
+# x = 10
+
+
+
+
+
+8219836118
+
+
+
+
+
+
+
+
+
---
+++ 45 days 11 june 2025/Lecture-3.1-core-functions.py.py
@@ -0,0 +1,163 @@
+# Core functions:
+#     1) print()
+#     2) type()
+#     3) input()
+#     ---> Concatenation
+#     ---> Type Casting
+    
+    
+#     Objects:
+#         1) Ittrable objects
+#             string, list, tuple, set, dictionry
+        
+#         2) Non Ittrable
+#             integer, float, boolean, complex
+            
+# indexing, slicing, for loop, len(), count(), 
+
+
+
+# 4) Len()
+    # --> help us to get the totole number of eleents a veriable hold
+
+
+# syntax:
+
+# len(veriable_name)
+
+
+# x = "Hellothis is a good car "
+
+# print(len(x))
+
+
+# zx = "this is done"
+
+# print(len(zx))
+
+
+
+# x = 12937
+
+# b = str(x)
+
+# print(len(b))
+
+
+
+# 5) count()
+#     ---> help us to get how many times a particuler element occur 
+
+
+# syntax:
+
+# variable_name.count("find")
+
+
+# xy = "this is a car and this is a good car and bus is also good"
+
+# x = xy.count("car")
+
+# print(x)
+
+
+
+
+# zx = 100020230300400004500340
+
+# # count  number of zero's
+
+# pq = str(zx)
+
+# print(pq.count("0"))
+
+
+
+
+# Question:
+    
+# data = ? this is a car
+
+# find_count = ? is
+
+# output:
+#     totel len of data is 12 and your element -->is>-- occur 2 times
+
+
+# data = input("Enter your text : ")
+
+# find_cout = input("What you wana find : ")
+
+# cou = data.count(find_cout)
+
+# le = len(data)
+
+# print(f"totel nubr of elements are {le} and your element --> {find_cout} <-- occure {cou} timees.")
+
+
+
+# Indexing:
+#     ---> to get an element from you ittrable object
+
+
+# x = "this is a car and good car"
+
+
+# indeixng syntax
+
+# veriable_name[iddex_number]
+
+# print(x[1])
+
+# negitive indexing:
+
+# print(x[-1])
+
+
+
+# Slicing:
+
+# zx = "Python is a good progrmmming language."
+
+# syntax:
+
+# veriable_name[start : end : increment]
+
+# deafult values:
+#     start --> 0
+#     end ---> n - 1
+#     increment ---> 1
+
+# print(zx[0 : 9])
+
+# print(zx[17 : 33])
+
+# print(zx[17 : 33 : 2])
+
+
+
+# zx = "PHP and Python use for Web devlopment."
+
+
+# print(zx[8 : ])
+
+# print(zx[8 : : 2])
+
+
+
+# zx = "PHP and Python use for Web devlopment."
+
+# print(zx[ : : 2])
+
+
+zx = "PHP and Python use for Web devlopment."
+
+print(zx[-11 : ])
+
+
+print(zx[-15 : -11])
+
+
+
+
+
