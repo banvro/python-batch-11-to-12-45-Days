944877e104afac10218497357ffafd115023b283
Naresh Kumar <72598101+banvro@users.noreply.github.com>
2025-07-28 13:12:12 +0530

    Create lecture-3-1-python-operators.py

---
+++ 45 days 11 june 2025/lecture-3-1-python-operators.py
@@ -0,0 +1,291 @@
+# Core function:
+
+# Operators in Python
+
+# x = 10 + 20
+
+# veriable --->  x
+
+# operators ---> =, +
+
+# operands ---> 10, 20
+
+
+
+# 1) Arithmatic Operators:
+#     1) +
+#     2) -
+#     3) *
+#     4) /
+#     5) //
+#     6) % 
+#     7) **
+
+
+# print(10 + 20)
+
+# print(20 - 3)
+
+# print(13 * 3)
+
+
+
+# print("100" + "20")
+
+# print("10000" * 20)
+
+# print(10000 * 20)
+
+
+# 1) Arithmatic Operators:
+#     1) +
+#     2) -
+#     3) *
+#     4) /
+#     5) //
+#     6) % 
+#     7) **
+
+
+# print(10 / 5)
+
+# print(10 / 3)
+
+# print(23 / 0)
+
+# 5) // --> floor devsion
+
+# print(10 / 3)
+
+# print(10 // 3)
+
+
+
+# 6) % ---> Modulus
+
+# jisse devide krr ree oo
+
+# reminder
+
+# print(15 % 2)
+
+# print(20 % 7)
+
+
+# 7) ** ---> exponential operator
+
+
+
+# print(3 ** 2)
+
+# print(5 ** 3)
+
+
+
+
+
+# ------------------------------------------
+
+
+# 2) Assignment Operators:
+#     1) =
+#     2) +=
+#     3) -=
+#     4) *=
+#     5) /=
+#     6) //=
+#     7) %=
+#     8) **=
+
+# x = 10
+
+# # x += 10  
+
+# x = x + 23
+
+# print(x)
+
+
+# 4) *=
+
+# q = 4
+
+# q *= 7
+
+# # q = q * 7
+
+# print(q)
+
+
+# Compersion Operators:
+#     1) ==
+#     2) !=
+#     3) >
+#     4) <
+#     5) >=
+#     6) <=
+    
+#     True, False
+
+
+# print(10 == 20)
+
+# print(20 == 20)
+
+# print("100" == 100)
+
+
+#  2) != ----> not equal to operator
+
+# print(10 != 20)
+
+# print(20 != 20)
+
+# print(100 != 99)
+
+# print("hello" != "hello")
+
+
+
+# 3) >
+
+# print(10 > 9)
+
+# print(100 > 110)
+
+# print(90 > 90)
+
+
+# <
+
+# print(10 < 100)
+
+
+
+# 6) >=
+
+# print(10 >= 10)
+
+# print(100 <= 20)
+
+
+# <=
+
+
+# ---------------------------------------------
+
+# 3) Logical Operaotors
+
+# 1) and
+# 2) or
+# 3) not
+
+
+# and Operaotor
+
+# 1    0    ---->  0
+# 0    1 ------> 0
+# 0    0  ------> 0
+# 1    1 ------> 1
+
+
+a = 10
+b = 20
+c = 10
+
+# print(a == c and b == c)
+
+# print(a < b and c == a)
+
+# print(a < b and c == a and a == b)
+
+
+# 2) OR Operator (+)
+
+
+# 0   0  -----> 0
+# 1   0  -----> 1
+# 0   1  -----> 1
+# 1   1  -----> 1
+
+
+# a = 10
+# b = 20
+# c = 10
+
+# print(a != c or a < 20)
+
+# print(a < 20 or b == a or c == a)
+
+
+# 3) not opertor
+
+# 0 ----> 1
+# 1 ----> 0
+
+
+# print(not(10 == 10))
+
+
+# print(not(10 != 10))
+
+
+
+# --------------------------------------------
+
+
+# 4) Indentity Operators:
+#     1) is
+#     2) not is
+
+
+# a = 10
+# b = 10
+
+# print(a is b)
+
+# a = "hellooo"
+
+# b = "helloo"
+
+# print(a is not b)
+
+
+# --------------------------------------------
+
+# 5) Mambership Operators
+
+# 1) in
+# 2) not in
+
+
+# x = "this is a car and this is a good car"
+
+
+# print("good" in x)
+
+
+# print("train" not in x)
+
+
+
+
+# ----------------------------------
+# Ternary operaotrs ---> condation based operaots
+
+
+# Syntax
+
+
+# True if condation else flase
+
+
+# Voating system
+
+age = 10
+
+print("ELigible" if age > 18 else "Not eligible")
+
+
+
+
