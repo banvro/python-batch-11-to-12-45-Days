2f10f6bbd968a18ef2e9b9ea68411dc3cf66cf08
Naresh Kumar <72598101+banvro@users.noreply.github.com>
2025-09-10 15:50:20 +0530

    Add files via upload

---
+++ django september batch/Lecture-2 -my first web app/Blog/Blog/asgi.py
@@ -0,0 +1,24 @@
+"""
+ASGI config for Blog project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Blog.settings')
+
+application = get_asgi_application()
+
+
+
+
+
+# http / https
+
+# ws / wss     ---> web socket
