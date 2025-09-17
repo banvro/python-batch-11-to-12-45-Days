63c5c472e8ab0379e114a530882e273ac80bf574
Naresh Kumar <72598101+banvro@users.noreply.github.com>
2025-09-17 16:39:58 +0530

    Add files via upload

---
+++ django september batch/Lecture-8 show data/MyBlog/blogs/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+
+# Create your models here.
+
+class ContactUs(models.Model):
+    full_name = models.CharField(max_length=100)
+    phone_number = models.IntegerField()
+    email_address = models.CharField(max_length=250)
+    message = models.TextField()
+
+    def __str__(self):
+        return f"Username is : {self.full_name} and phon number is {self.phone_number}"
+
+
+ 
+
+
+choice = [
+    ("Frontend Devloper", "Frontend Devloper"),
+    ("Backend Devloper", "Backend Devloper"),
+    ("Tester", "Tester"),
+    ("Full-Stack Devloper", "Full-Stack Devloper")
+    ]
+
+class Employee(models.Model):
+    e_name = models.CharField(max_length=50)
+    e_branch = models.CharField(max_length=50, choices = choice, default = "Backend Devloper")
+    e_p_num = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.e_name} --> {self.e_branch}"
---
+++ django september batch/Lecture-8 show data/MyBlog/blogs/urls.py
@@ -0,0 +1,24 @@
+from django.urls import path
+from blogs import views
+
+urlpatterns = [
+    path("", views.homee, name = "home"), 
+    path("about-us", views.aboutus, name = "about"),
+    path("contact-us", views.contactus, name = "contact"),
+    path("services", views.services, name = "services"),
+
+    # my authanication pagess....
+    path("login-page", views.loginn, name = "login"),
+    path("sign-up", views.signuppage, name = "signup"),
+
+    # svaing contact us form
+    path("save-form-data", views.savinginfo, name = "saving"),
+
+    # for adding employe from
+    path("add-employee", views.adding_employee, name = "addingemp"),
+
+    # showing data
+    path("all-contact-us-forms", views.allcontactus, name = "allforms"),
+    path("all-employee", views.allemployee, name = "allemp"),
+]
+ 
